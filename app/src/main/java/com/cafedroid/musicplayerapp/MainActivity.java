57490733aa15ddd5b47aab9ca79c9db39f5bdfd7
RahulKathuria <rahulkathuria52@gmail.com>
2018-12-27 01:39:25 +0530

    Added List items

--- app/src/main/java/com/cafedroid/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/cafedroid/musicplayerapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.cafedroid.musicplayerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    TextView textView;
+    String[] listItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
+
+
+
+
 }
