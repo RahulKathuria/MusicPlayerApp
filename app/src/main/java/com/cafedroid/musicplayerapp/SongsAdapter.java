3f46b798f6e91699db59ef0add911d7334ad3c6b
RahulKathuria <rahulkathuria52@gmail.com>
2018-12-30 03:41:06 +0530

    Layout inflated

--- app/src/main/java/com/cafedroid/musicplayerapp/SongsAdapter.java
+++ app/src/main/java/com/cafedroid/musicplayerapp/SongsAdapter.java
@@ -3,6 +3,7 @@ package com.cafedroid.musicplayerapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -17,6 +18,11 @@ public class SongsAdapter extends ArrayAdapter<Songs> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if(listItemView==null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
+
+        }
+        return listItemView;
     }
 }
