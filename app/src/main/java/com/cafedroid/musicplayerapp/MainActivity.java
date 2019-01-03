bee973f18eee176e2eca2cb5bfcda42eafe2fefd
RahulKathuria <rahulkathuria52@gmail.com>
2019-01-04 02:43:46 +0530

    Updated MainActivity

--- app/src/main/java/com/cafedroid/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/cafedroid/musicplayerapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         prateek.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,NowPlaying.class);
+                Intent i = new Intent(MainActivity.this,PrateekSongs.class);
                 startActivity(i);
             }
 
